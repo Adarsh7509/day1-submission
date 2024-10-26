e4d119322385c5475e82cf08aef30d271a5e0a2a
adarsh sharma <adarshpurushottam2002@gmail.com>
2024-10-27 01:50:06 +0530

    Add files via upload

---
+++ Day3-4Solution/Day3-4Solution/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Day3_4Solution;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello team!");
+        // AuthorBook b = new  AuthorBook();
+        // b.BookAuthor();
+        //var app = new Customercls();
+        //app.Run();
+        new TransportApp().Run();
+    }
+}
